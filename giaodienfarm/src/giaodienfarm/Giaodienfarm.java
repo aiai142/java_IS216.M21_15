77eae47447a4581eb2687be863603d8cac278d6e
Ngoc Linh <linhtn0211@gmail.com>
2022-05-21 13:17:16 +0700

    Ngoc Linh create GUI for customer's home page

---
+++ giaodienfarm/src/giaodienfarm/Giaodienfarm.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package giaodienfarm;
+
+/**
+ *
+ * @author ACER
+ */
+public class Giaodienfarm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new HomePage_Customer().setVisible(true);
+        // TODO code application logic here
+        
+        
+    }
+    
+}
