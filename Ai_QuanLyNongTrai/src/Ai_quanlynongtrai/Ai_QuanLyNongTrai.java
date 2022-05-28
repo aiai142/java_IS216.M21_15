0ab894f4a94785a039492f7fb3ab8053ad683bae
aiai142 <20520368@gm.uit.edu.vn>
2022-05-28 13:12:51 +0700

    cnnt

--- Ai_QuanLyNongTrai/src/Ai_quanlynongtrai/Ai_QuanLyNongTrai.java
+++ Ai_QuanLyNongTrai/src/Ai_quanlynongtrai/Ai_QuanLyNongTrai.java
@@ -26,9 +26,7 @@ public class Ai_QuanLyNongTrai {
      */
     public static void main(String[] args){
         // TODO code application logic here
-     java.awt.EventQueue.invokeLater(() -> {
-             new AdminHome().setVisible(true);
-     });
+     
        }
       }  
     
