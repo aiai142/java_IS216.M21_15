016d3ab2a314eee28c8347461b20a0a922951127
aiai142 <20520368@gm.uit.edu.vn>
2022-05-30 23:09:12 +0700

    lan 2

---
+++ Ai_QuanLyNongTrai/src/Ai_quanlynongtrai/Ai_QuanLyNongTrai.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Ai_quanlynongtrai;
+
+import ConnectDB.ConnectionUtils;
+import View.AdminHome;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.lang.model.util.Types;
+import java.sql.*;
+
+/**
+ *
+ * @author diuai
+ */
+public class Ai_QuanLyNongTrai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(() -> {
+            new AdminHome().setVisible(true);
+        });
+
+    }
+}
